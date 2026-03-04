4042871827f1c0c41abc2469553e9aaf0eef909f
Manatsawin Wanasit <m4natwynn@gmail.com>
2026-03-04 14:45:58 +0700

    Bug fix#2 – adding student id validation

--- validate.js
+++ validate.js
@@ -1,11 +1,14 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
+	let sid = (document.getElementById("sid").value).trim();
+  
+	let sidPattern = /^[0-9]{10}$/;
+  
+	if (sidPattern.test(sid)) {
+	  return true;
+	} else {
+	  return false;
+	}
   }
-}
 
 function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
